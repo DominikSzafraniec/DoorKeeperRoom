a4dc04e94d5938d45e97eff2faed52ab262f29f0
Dominik <japanjestem@o2.pl>
2021-11-18 11:59:44 +0100

    Key Dispose, Handing Key Delete

--- DoorKeeperRoom/Views/HandingView.xaml.cs
+++ DoorKeeperRoom/Views/HandingView.xaml.cs
@@ -4,6 +4,8 @@ using System.Data.Entity;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using Prism.Commands;
+using DoorKeeperRoom.Models;
 
 namespace DoorKeeperRoom.Views
 {
@@ -14,10 +16,12 @@ namespace DoorKeeperRoom.Views
 	{
 		private Models.DoorKeeperRoom_DBConnection dataEntities = new Models.DoorKeeperRoom_DBConnection();
 		CollectionViewSource khViewSource;
+		CollectionViewSource khAll;
 		public HandingView()
 		{
 			InitializeComponent();
 			khViewSource = (CollectionViewSource)FindResource("key_handingViewSource");
+			khAll = (CollectionViewSource)FindResource("key_handingAll");
 			DataContext = this;
 		}
 
@@ -27,16 +31,41 @@ namespace DoorKeeperRoom.Views
 			khViewSource.Source = dataEntities.key_handing.Local;
 		}
 
+		private DelegateCommand<string> _updateKeyHandingRecord;
+		public DelegateCommand<string> UpdateKeyHandingRecord =>
+			_updateKeyHandingRecord ?? (_updateKeyHandingRecord = new DelegateCommand<string>(UpdateKeyHandingCommand));
+
+		void UpdateKeyHandingCommand(string s)
+		{
+			dataEntities.SaveChanges();
+		}
+
+		private DelegateCommand<string> _deleteKeyHandingRecord;
+		public DelegateCommand<string> DeleteKeyHandingRecord =>
+		_deleteKeyHandingRecord ?? (_deleteKeyHandingRecord = new DelegateCommand<string>(DeleteKeyHandingCommand));
+
+		void DeleteKeyHandingCommand(string s)
+		{
+			var selected = key_handingDataGrid.SelectedItem as key_handing;
+			if (MessageBox.Show("Usuwając pracownika usuwasz całą historię pobieranych przez niego kluczy. Czy na pewno chcesz usunąć pracownika?", "Usuwanie pracownika", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+			{
+				dataEntities.key_handing.Remove(selected);
+				dataEntities.SaveChanges();
+				khViewSource.View.Refresh();
+			}
+		}
+
 		private void key_handingListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
-			/*
-			var selectedRecord = (Models.key_handing)key_handingListView.SelectedItem;
-			idTextBoxEdit.Text = selectedRecord.id.ToString();
-			id_keyTextBoxEdit.Text = selectedRecord.id_key.ToString();
-			id_workerTextBoxEdit.Text = selectedRecord.id_worker.ToString();
-			handing_dateDatePickerEdit.SelectedDate=selectedRecord.handing_date;
-			return_dateDatePickerEdit.SelectedDate = selectedRecord.return_date;
-			*/
+			
+			var selectedRecord = (Models.key_handing)key_handingDataGrid.SelectedItem;
+			var worker = dataEntities.workers.Find(selectedRecord.id_worker);
+			var key = dataEntities.keys.Find(selectedRecord.id_key);
+		}
+		private void getJoiningTable()
+		{
+			//var query = from hk in dataEntities.key_handing
+			//			join w in dataEntities.workers on hk.id_worker equals w.id_worker
 		}
 	}
 }
--- DoorKeeperRoom/Views/KeyDisposeView.xaml.cs
+++ DoorKeeperRoom/Views/KeyDisposeView.xaml.cs
@@ -6,6 +6,8 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using Prism.Commands;
 using System.Linq;
+using DoorKeeperRoom.Models;
+using System;
 
 namespace DoorKeeperRoom.Views
 {
@@ -49,52 +51,248 @@ namespace DoorKeeperRoom.Views
 				mess += "\nNumer klucza jest wymagany";
 			}
 			if(!reqEmpty)
-			{
-				var wk = (from workers in dataEntities.workers.Local
-							where workers.name.Equals(workerNameTextBox.Text) && workers.lastname.Equals(workerLastnameTextBox.Text)
-							select workers);
-				if (wk != null)
+			{	
+				var wkAll = dataEntities.workers.Where(w => w.name == workerNameTextBox.Text && w.lastname == workerLastnameTextBox.Text);
+				if (wkAll.Any())
 				{
-					if (wk.Count()==1)
+					if (wkAll.Count()==1)
 					{
 
-						workerIdTextBox.Text = wk.First().id_worker.ToString();
-					} else
+						workerIdTextBox.Text = wkAll.First().id_worker.ToString();
+					} 
+					else
 					{
-						reqEmpty = true;
-						mess = "Jest kilku pracowników o takim imieniu i nazwisku. Podaj dodatkowo numer pracownika."+ workerNameTextBox.Text + wk.Count().ToString();
-						workerIdTextBox.Visibility = Visibility.Visible;
-						workerIdLabel.Visibility = Visibility.Visible;
+						var dialog = new GetWorkerId();
+						
+						if (dialog.ShowDialog() == true)
+						{
+							if (dialog.ResponseText.Length == 4)
+							{
+								try
+								{
+									var id = Int32.Parse(dialog.ResponseText);
+									var wk = wkAll.Where(w => w.id_worker == id);
+									if (wk.Any())
+									{
+										workerIdTextBox.Text = wk.First().id_worker.ToString();
+									}
+									else
+									{
+										reqEmpty = true;
+										mess = "Nie ma pracowanika o imieniu " + workerNameTextBox.Text + " nazwisku " + workerLastnameTextBox + " i numerze pracownika " + dialog.ResponseText + "!";
+									}
+								}
+								catch (Exception e)
+								{
+									reqEmpty = true;
+									mess = "Wprowadź numer pracownika składający się wyłącznie z cyfr!";
+								}
+							}
+							else
+							{
+								reqEmpty = true;
+								mess = "Nie wpisano poprawnego numeru pracownika!";
+							}							
+						}
 					}
+				}
+				else
+				{
+					reqEmpty = true;
+					mess = "Nie ma pracowanika o imieniu "+workerNameTextBox.Text+" i nazwisku "+ workerLastnameTextBox +"!";
 				}
 
 			}
-			if (reqEmpty)
+			if (!reqEmpty)
 			{
-				MessageBox.Show(mess, "Wydawanie klucza", MessageBoxButton.OK, MessageBoxImage.None);
+				if (keyIdTextBox.Text.Length == 4)
+				{
+					try
+					{
+						var idk = Int32.Parse(keyIdTextBox.Text);
+						var kf = dataEntities.keys.Find(idk);
+						if (kf != null)
+						{
+							var keyAv = dataEntities.key_handing.Where(k => k.id_key == idk && k.return_date == null);
+							if (keyAv.Any())
+							{
+								reqEmpty = true;
+								mess = "Klucz jest już pobrany przez pracownika nr:" + keyAv.First().id_worker;
+							} else
+							{
+								dataEntities.key_handing.Add(new key_handing() { id_key = idk, id_worker = int.Parse(workerIdTextBox.Text), handing_date = DateTime.Now });
+								dataEntities.SaveChanges();
+								
+							}
+						}
+					}
+					catch (Exception e)
+					{
+						reqEmpty = true;
+						mess = "Wprowadź numer klucza składający sie wyłącznie z cyfr!";
+					}
+				}
+				else
+				{
+					reqEmpty = true;
+					mess = "Nie wpisano poprawnego numeru klucza!";
+				}
 			}
 			else
 			{
 
 
 			}
+			if(reqEmpty)
+				MessageBox.Show(mess,"Wydawanie klucza",MessageBoxButton.OK, MessageBoxImage.Warning);
+			else
+				MessageBox.Show("Wydano klucz pracownikowi", "Wydawanie klucza", MessageBoxButton.OK, MessageBoxImage.Warning);
+		}
 
-			//	if (!reqEmpty)
-			//{
-			//	keys newKeyAdd = new keys
-			//	{
-			//		room_name = room_nameTextBoxAdd.Text,
-			//	};
-			//	object p = dataEntities.keys.Add(newKeyAdd);
-			//	dataEntities.SaveChanges();
-			//	kViewSource.Source = dataEntities.keys.Local;
-			//	kViewSource.View.Refresh();
-			//	ClearNewKeyCommand("s");
-			//}
-			//else
-			//{
-				
-			//}
+		private DelegateCommand<string> _receiveKey;
+		public DelegateCommand<string> ReceiveKey =>
+		_receiveKey ?? (_receiveKey = new DelegateCommand<string>(ReceiveKeyCommand));
+
+		void ReceiveKeyCommand(string s)
+		{
+			var mess = "";
+			var reqEmpty = false;
+			if (workerNameTextBox.Text == "")
+			{
+				reqEmpty = true;
+				mess += "\nImię jest wymagane";
+			}
+			if (workerLastnameTextBox.Text == "")
+			{
+				reqEmpty = true;
+				mess += "\nNazwisko jest wymagane";
+			}
+			if (keyIdTextBox.Text == "")
+			{
+				reqEmpty = true;
+				mess += "\nNumer klucza jest wymagany";
+			}
+			if (!reqEmpty)
+			{
+				var wkAll = dataEntities.workers.Where(w => w.name == workerNameTextBox.Text && w.lastname == workerLastnameTextBox.Text);
+				if (wkAll.Any())
+				{
+					if (wkAll.Count() == 1)
+					{
+
+						workerIdTextBox.Text = wkAll.First().id_worker.ToString();
+					}
+					else
+					{
+						var dialog = new GetWorkerId();
+
+						if (dialog.ShowDialog() == true)
+						{
+							if (dialog.ResponseText.Length == 4)
+							{
+								try
+								{
+									var id = Int32.Parse(dialog.ResponseText);
+									var wk = wkAll.Where(w => w.id_worker == id);
+									if (wk.Any())
+									{
+										workerIdTextBox.Text = wk.First().id_worker.ToString();
+									}
+									else
+									{
+										reqEmpty = true;
+										mess = "Nie ma pracowanika o imieniu " + workerNameTextBox.Text + " nazwisku " + workerLastnameTextBox + " i numerze pracownika " + dialog.ResponseText + "!";
+									}
+								}
+								catch (Exception e)
+								{
+									reqEmpty = true;
+									mess = "Wprowadź numer pracownika składający się wyłącznie z cyfr!";
+								}
+							}
+							else
+							{
+								reqEmpty = true;
+								mess = "Nie wpisano poprawnego numeru pracownika!";
+							}
+						}
+					}
+				}
+				else
+				{
+					reqEmpty = true;
+					mess = "Nie ma pracowanika o imieniu " + workerNameTextBox.Text + " i nazwisku " + workerLastnameTextBox + "!";
+				}
+
+			}
+			if (!reqEmpty)
+			{
+				if (keyIdTextBox.Text.Length == 4)
+				{
+					try
+					{
+						var idk = Int32.Parse(keyIdTextBox.Text);
+						var kf = dataEntities.keys.Find(idk);
+						if (kf != null)
+						{
+							var keyAv = dataEntities.key_handing.Where(k => k.id_key == idk && k.return_date == null);
+							if (keyAv.Any())
+							{
+								var idkk = int.Parse(workerIdTextBox.Text);
+								var wkKey = keyAv.Where(k => k.id_worker == idkk);
+								if (wkKey.Any())
+								{
+									wkKey.First().return_date = DateTime.Now;
+									dataEntities.SaveChanges();
+								}
+								else
+								{
+									mess = "Klucz jest już pobrany przez pracownika nr:" + wkKey.First().id_worker + "\n tylko on może zdać klucz";
+								}
+							}
+							else
+							{
+								reqEmpty = true;
+								mess = "Klucz nie został pobrany";
+
+							}
+						}
+					}
+					catch (Exception e)
+					{
+						//Console.WriteLine(e.Message);
+						reqEmpty = true;
+						mess = "Wprowadź numer klucza składający sie wyłącznie z cyfr!";
+					}
+				}
+				else
+				{
+					reqEmpty = true;
+					mess = "Nie wpisano poprawnego numeru klucza!";
+				}
+			}
+			else
+			{
+
+
+			}
+			if (reqEmpty)
+				MessageBox.Show(mess, "Odbieranie klucza", MessageBoxButton.OK, MessageBoxImage.Warning);
+			else
+				MessageBox.Show("Odebrano klucz od pracownika", "Odbieranie klucza", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+		}
+
+		private DelegateCommand<string> _clearForm;
+		public DelegateCommand<string> ClearForm =>
+		_clearForm ?? (_clearForm = new DelegateCommand<string>(ClearFormCommand));
+
+		void ClearFormCommand(string s)
+		{
+			workerIdTextBox.Text = "";
+			workerNameTextBox.Text = "";
+			workerLastnameTextBox.Text = "";
+			keyIdTextBox.Text = "";
 		}
 
 	}
